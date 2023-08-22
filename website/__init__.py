dd2e5a11eb76e3a484809bb39be058034e17b085
quadgeter <quadgeter@gmail.com>
2023-08-22 00:59:07 -0400

    tweaks for deployment

--- website/__init__.py
+++ website/__init__.py
@@ -9,7 +9,8 @@ DB_NAME = "playerDB.db"
 def createApp():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "IV"
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://jqezfrngzgqqxk:8c2fd19ab3fca5815d638d25980bcdceeb7cb0d1c57647bced97cbcfa2c22f30@ec2-3-232-218-211.compute-1.amazonaws.com:5432/d5om337l1m5vv5'
+    # app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'        #sqlite database
     db.init_app(app)
 
     from .views import views
