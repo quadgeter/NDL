99949d8116fa7b607cac1821a40cf64f477cc82e
quadgeter <quadgeter@gmail.com>
2024-08-25 18:59:46 -0400

    updated db

--- website/__init__.py
+++ website/__init__.py
@@ -9,7 +9,7 @@ DB_NAME = "-------"
 def createApp():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "--"
- #   app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://-----'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://wlopnykscbmebr:92b1151924463e7ced766b677ba8104d4bf374b0f03625c78c5edcc84627e9e9@ec2-44-199-147-86.compute-1.amazonaws.com:5432/d4suoqn8lh2sq0'
   #  app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'        #sqlite database
     db.init_app(app)
 
